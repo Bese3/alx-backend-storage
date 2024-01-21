7378565cccdb67209446f0ae293b0076206d6444
besufikad <07yilmabese@gmail.com>
2024-01-21 16:21:28 +0300

    t 6

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -2,24 +2,21 @@
 -- correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
-CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score FLOAT)
 BEGIN
-    DECLARE project_count INT DEFAULT 0;
-    DECLARE project_id INT DEFAULT 0;
+    DECLARE project_count INT;
+    DECLARE project_id INT;
 
-    SELECT COUNT(id)
-        INTO project_count
-        FROM projects
-        WHERE name = project_name;
+    SET project_count = (SELECT COUNT(*) FROM projects
+    WHERE name = project_name);
+
     IF project_count = 0 THEN
-        INSERT INTO projects(name)
-            VALUES(project_name);
+        INSERT INTO projects (name) VALUES (project_name);
     END IF;
-    SELECT id
-        INTO project_id
-        FROM projects
-        WHERE name = project_name;
-    INSERT INTO corrections(user_id, project_id, score)
-        VALUES (user_id, project_id, score);
+
+    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, project_id, score);
 END $$
 DELIMITER ;
